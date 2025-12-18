226e622b5be49fd8908d380f38b6bceab301e243
sudarshangrg <sudarshangrg36@gmail.com>
2025-12-18 15:29:49 +0545

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(filepath:str)->List[str]:
+    '''
+    This returns the requirements 
+    '''
+    requirements=[]
+    with open(filepath) as obj:
+        requirements=obj.readlines()
+        requirements=[requirements.replace('\n','') for req in requirements]
+
+        if requirements == HYPHEN_E_DOT:
+            requirements.remove(HYPHEN_E_DOT)
+
+setup(
+    name = 'mlproject',
+    version= '0.0.1',
+    author='Sudarshan',
+    author_email='sudarshangrg36@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)

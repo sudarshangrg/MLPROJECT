585526f90d6e19fb26394aa0482068ef83d6b85b
sudarshangrg <sudarshangrg36@gmail.com>
2026-02-20 15:19:26 +0545

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -26,12 +26,12 @@ class ModelTrainerConfig:
 
 class ModelTrainer:
     def __init__(self):
-        self.mdel_trainer_config=ModelTrainerConfig()
+        self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+    def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split train and test input data")
-            x_train,y_train,x_test,y_test=(
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:,-1],
                 train_array[:,-1],
                 test_array[:,:,-1],
@@ -50,5 +50,25 @@ class ModelTrainer:
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models)
 
-        except:
-            pass
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("NO best mdoel found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test,predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e,sys)
